57f5d2006fa56896b52d732c0390f6ae8199733b
gsl-anthonymerle <anthony.merle@groupeseloger.com>
2024-02-06 11:52:15 +0100

    add tests

--- Tests/GildedRoseTests/GildedRoseTests.swift
+++ Tests/GildedRoseTests/GildedRoseTests.swift
@@ -2,10 +2,103 @@
 import XCTest
 
 class GildedRoseTests: XCTestCase {
-    func testFoo() throws {
-        let items = [Item(name: "foo", sellIn: 0, quality: 0)]
-        let app = GildedRose(items: items)
+    func testRegularItem() {
+        assertItemUpdate(
+            Item(name: "Regular Item", sellIn: 10, quality: 10),
+            expectedQuality: 9,
+            expectedSellIn: 9
+        )
+    }
+
+    func testRegularItemWithQualityIs0() {
+        assertItemUpdate(
+            Item(name: "Regular Item", sellIn: 10, quality: 0),
+            expectedQuality: 0,
+            expectedSellIn: 9
+        )
+    }
+
+    func testRegularItemWithSellInLessThan0() {
+        assertItemUpdate(
+            Item(name: "Regular Item", sellIn: 0, quality: 10),
+            expectedQuality: 8,
+            expectedSellIn: -1
+        )
+    }
+
+    func testAgedBrie() {
+        assertItemUpdate(
+            Item(name: "Aged Brie", sellIn: 10, quality: 10),
+            expectedQuality: 11,
+            expectedSellIn: 9
+        )
+    }
+
+    func testAgedBrieWithQualityIs50() {
+        assertItemUpdate(
+            Item(name: "Aged Brie", sellIn: 10, quality: 50),
+            expectedQuality: 50,
+            expectedSellIn: 9
+        )
+    }
+
+    func testAgedBrieWithSellInLessThan0() {
+        assertItemUpdate(
+            Item(name: "Aged Brie", sellIn: 0, quality: 10),
+            expectedQuality: 12,
+            expectedSellIn: -1
+        )
+    }
+
+    func testBackstagePass() {
+        assertItemUpdate(
+            Item(name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 20, quality: 10),
+            expectedQuality: 11,
+            expectedSellIn: 19
+        )
+    }
+
+    func testBackstagePassIsLessThan11() {
+        assertItemUpdate(
+            Item(name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 10, quality: 10),
+            expectedQuality: 12,
+            expectedSellIn: 9
+        )
+    }
+
+    func testBackstagePassWithSellInLessThan0() {
+        assertItemUpdate(
+            Item(name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 0, quality: 10),
+            expectedQuality: 0,
+            expectedSellIn: -1
+        )
+    }
+
+    func testBackstagePassWithSellInLessThan6() {
+        assertItemUpdate(
+            Item(name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 5, quality: 10),
+            expectedQuality: 13,
+            expectedSellIn: 4
+        )
+    }
+
+    func testSulfuras() {
+        assertItemUpdate(
+            Item(name: "Sulfuras, Hand of Ragnaros", sellIn: 5, quality: 80),
+            expectedQuality: 80,
+            expectedSellIn: 5
+        )
+    }
+
+    private func assertItemUpdate(
+        _ item: Item,
+        expectedQuality: Int,
+        expectedSellIn: Int
+    ) {
+        let app = GildedRose(items: [item])
         app.updateQuality()
-        XCTAssertEqual(app.items[0].name, "fixme")
+
+        XCTAssertEqual(app.items[0].quality, expectedQuality)
+        XCTAssertEqual(app.items[0].sellIn, expectedSellIn)
     }
 }

15dabf359f50aa143463824ea0c434a309100388
gsl-anthonymerle <anthony.merle@groupeseloger.com>
2024-02-06 11:01:56 +0100

    add adjustQuality to Item

--- Sources/GildedRose/GildedRose.swift
+++ Sources/GildedRose/GildedRose.swift
@@ -10,23 +10,23 @@ public class GildedRose {
             if items[i].name != "Aged Brie", items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
                 if items[i].quality > 0 {
                     if items[i].name != "Sulfuras, Hand of Ragnaros" {
-                        items[i].quality = items[i].quality - 1
+                        items[i].updateQuality(difference: -1)
                     }
                 }
             } else {
                 if items[i].quality < 50 {
-                    items[i].quality = items[i].quality + 1
+                    items[i].updateQuality(difference: 1)
 
                     if items[i].name == "Backstage passes to a TAFKAL80ETC concert" {
                         if items[i].sellIn < 11 {
                             if items[i].quality < 50 {
-                                items[i].quality = items[i].quality + 1
+                                items[i].updateQuality(difference: 1)
                             }
                         }
 
                         if items[i].sellIn < 6 {
                             if items[i].quality < 50 {
-                                items[i].quality = items[i].quality + 1
+                                items[i].updateQuality(difference: 1)
                             }
                         }
                     }
@@ -42,18 +42,37 @@ public class GildedRose {
                     if items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
                         if items[i].quality > 0 {
                             if items[i].name != "Sulfuras, Hand of Ragnaros" {
-                                items[i].quality = items[i].quality - 1
+                                items[i].updateQuality(difference: -1)
                             }
                         }
                     } else {
-                        items[i].quality = items[i].quality - items[i].quality
+                        items[i].quality = 0
                     }
                 } else {
                     if items[i].quality < 50 {
-                        items[i].quality = items[i].quality + 1
+                        items[i].updateQuality(difference: 1)
                     }
                 }
             }
         }
+    }
+}
+
+/*
+ Kevlin Henney
+ https://youtu.be/NMPeAW2RWdc?si=jGAhX7PB6jiorRB_&t=1438
+
+ Feature (what the code does)
+ Performance (amount of resources (time, memory, etc..) needed by the code)
+ Readability/Maintainability/Scalability
+
+ Feature addition => +Feature, ~=Performance , ~=Readability
+ Optimization => =Feature, +Performance , ~=Readability
+ Factoring => =Feature, ~=Performance , +Readability
+ */
+
+extension Item {
+    func updateQuality(difference: Int) {
+        quality += difference
     }
 }
